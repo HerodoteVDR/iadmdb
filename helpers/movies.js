6b969b4ebad1bf7015424483a70b423c9c14cd2c
Théodore van der Rest <66850239+HerodoteVDR@users.noreply.github.com>
2022-01-10 21:50:06 +0100

    Add files via upload

--- helpers/movies.js
+++ helpers/movies.js
@@ -2,10 +2,6 @@ const axios = require("axios");
 const tmdb_apiKey ="d142929db40137ccd06e817b947269c0";
 const shuffle = require("lodash/shuffle");
 
-let movieId = 550;
-movieId.toString();
-
-
 exports.getRandomMovies = function () {
     let discoverMovieUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${tmdb_apiKey}`;
     let mdbData = axios.get(discoverMovieUrl);
